d32921ca382d7dd918ae6fbf54ac107a9e003cdc
Ryuta Kono <otami0103@gmail.com>
2020-09-25 17:03:39 +0900

    add status method

--- queue/include/thread_safe_queue.hpp
+++ queue/include/thread_safe_queue.hpp
@@ -1,5 +1,6 @@
 #include <mutex>
 #include <queue>
+#include <exception>
 
 namespace thread_safe
 {
@@ -20,11 +21,23 @@ namespace thread_safe
         {
             std::lock_guard<std::mutex> lock(m_mtx);
             if (m_queue.empty()) {
-                throw std::exception("empty queue");
+                throw std::runtime_error("empty queue");
             }
 
             val = m_queue.front();
             m_queue.pop();
+        }
+
+        bool empty()
+        {
+            std::lock_guard<std::mutex> lock(m_mtx);
+            return m_queue.empty();
+        }
+
+        size_t size()
+        {
+            std::lock_guard<std::mutex> lock(m_mtx);
+            return m_queue.size();
         }
 
     private:
--- queue/sample/sample.cpp
+++ queue/sample/sample.cpp
@@ -25,14 +25,40 @@ void executeThreads(int threadNum)
 
 void executeThreadsSafe(int threadNum)
 {
+    std::vector<std::thread> threads;
+
     thread_safe::Queue<int> queue;
+    auto add = [&]() {
+        for (int i = 0; i < 1000; i++) {
+            queue.push(i);
+        }
+    };
+
+    std::cout << "thread num:" << threadNum << std::endl;
+    for (int i = 0; i < threadNum; i++) {
+        threads.push_back(std::thread(add));
+    }
+    for (auto& th : threads) {
+        th.join();
+    }
+    std::cout << "queue size = " << queue.size() << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 5; i++) {
         //executeThreads(i);
         executeThreadsSafe(i);
+    }
+
+    thread_safe::Queue<double> que;
+    for (int i = 10; i > 0; i--) {
+        que.push((double)i / 100.0);
+    }
+    while (que.size() > 0) {
+        double val;
+        que.pop(val);
+        std::cout << val << ", " << que.size() << std::endl;
     }
     
     return 0;
