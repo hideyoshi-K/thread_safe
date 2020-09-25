b9631d96cacb92974e8c7be94446da9e532e8998
Ryuta Kono <otami0103@gmail.com>
2020-09-25 15:44:38 +0900

    add pop method

--- queue/include/thread_safe_queue.hpp
+++ queue/include/thread_safe_queue.hpp
@@ -16,6 +16,17 @@ namespace thread_safe
             m_queue.push(val);
         }
 
+        void pop(T& val)
+        {
+            std::lock_guard<std::mutex> lock(m_mtx);
+            if (m_queue.empty()) {
+                throw std::exception("empty queue");
+            }
+
+            val = m_queue.front();
+            m_queue.pop();
+        }
+
     private:
         std::mutex    m_mtx;
         std::queue<T> m_queue;
